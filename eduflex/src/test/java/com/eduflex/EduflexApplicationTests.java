5dd3ba6e1cbe55b62686f1ec3cc6639dd6de82d6
alexwest1981 <134130345+alexwest1981@users.noreply.github.com>
2026-02-26 15:54:01 +0100

    v3.4.6: Fixed CI SQL failures by isolating H2 test databases and cleaning up test configs.

--- eduflex/src/test/java/com/eduflex/EduflexApplicationTests.java
+++ eduflex/src/test/java/com/eduflex/EduflexApplicationTests.java
@@ -6,13 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @org.springframework.test.context.ActiveProfiles("test")
 @org.springframework.test.context.TestPropertySource(properties = {
-		"spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.jpa.hibernate.ddl-auto=create-drop",
-		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-		"spring.jpa.defer-datasource-initialization=true",
-		"spring.flyway.enabled=false",
-		"spring.jpa.properties.hibernate.multiTenancy=NONE"
+		"spring.flyway.enabled=false"
 })
 class EduflexApplicationTests {
 
--- eduflex/src/test/java/com/eduflex/backend/SchemaGenTest.java
+++ eduflex/src/test/java/com/eduflex/backend/SchemaGenTest.java
@@ -7,11 +7,6 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles({ "schema-gen", "test" })
 @org.springframework.test.context.TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-        "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.jpa.hibernate.ddl-auto=create",
-        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-        "spring.jpa.defer-datasource-initialization=true",
         "spring.flyway.enabled=false"
 })
 class SchemaGenTest {
